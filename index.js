e06931811ff28831a33e555814038daaf53f0dac
Josh <jrogan@wikia-inc.com>
2016-09-07 20:31:33 -0700

    Support module.exports

--- index.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const test = require('./dist/index.js');
-console.log(test);
-
-// let test = new BraviaRemoteControl('1', 3);
-// console.log(test);
--- src/BraviaRemoteControl.js
+++ src/BraviaRemoteControl.js
@@ -213,3 +213,5 @@ export default class BraviaRemoteControl {
 	}
 
 }
+
+module.exports = BraviaRemoteControl;
